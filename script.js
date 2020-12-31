ea72f4a4bac26ab067d0cbf8876e7a2dcadd3bea
Jorge Abdallah <jorgeea1205@gmail.com>
2020-12-31 00:49:45 -0400

    Fized margin issues and added test button

---
+++ script.js
@@ -0,0 +1,6 @@
+const orderSubmitBtn = document.getElementById('order-submit');
+
+
+orderSubmitBtn.addEventListener('click', () => {
+    window.open("https://wa.me/584121822719?text=Mensaje%20de%20prueba");
+})
