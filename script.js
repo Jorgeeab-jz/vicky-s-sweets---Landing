e79746666b5a9414a7d31d803437560e036170d7
Jorge Abdallah <jorgeea1205@gmail.com>
2021-01-01 11:22:42 -0400

    Changed test message for testing

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const orderSubmitBtn = document.getElementById('order-submit');
-
+let testStr = 'Pedido:\n3 Rol de canela 0.9$\n6 Porcion de torta de Zanahoria 3$\nTotal: 3.9$';
 
 orderSubmitBtn.addEventListener('click', () => {
-    window.open("https://wa.me/584121822719?text=Mensaje%20de%20prueba");
+    window.open(`https://wa.me/584121822719?text=${encodeURIComponent(testStr)}`);
 })
