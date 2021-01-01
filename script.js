4a17db67bcc7fcc9be10cee74875f1e6be2cc772
Jorge Abdallah <jorgeea1205@gmail.com>
2021-01-01 17:14:29 -0400

    Added modal with basic functionality for testing Organized files in folders

--- script.js
+++
@@ -1,6 +0,0 @@
-const orderSubmitBtn = document.getElementById('order-submit');
-let testStr = 'Pedido:\n3 Rol de canela 0.9$\n6 Porcion de torta de Zanahoria 3$\nTotal: 3.9$';
-
-orderSubmitBtn.addEventListener('click', () => {
-    window.open(`https://wa.me/584121822719?text=${encodeURIComponent(testStr)}`);
-})
